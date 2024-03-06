60a7a3ed489224343fbd3ca54eaf7523280268d1
thecoello <thecoello@gmail.com>
2024-03-06 14:23:37 +0100

    Configuración de permisos de CORS

--- ApiEvent/src/main/java/com/ApiEvent/config/SecurityConfig.java
+++ ApiEvent/src/main/java/com/ApiEvent/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig   {
@@ -30,6 +32,16 @@ public class SecurityConfig   {
             .httpBasic(Customizer.withDefaults());
      
         return http.build();
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
     
     @Bean
