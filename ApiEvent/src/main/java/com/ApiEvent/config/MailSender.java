f064003224fa07ddca15cbb5644ec32f4a5d2c9d
Sonia Mori <sonymm6s@gmail.com>
2024-03-12 19:41:06 +0100

    Eliminación de bean

--- ApiEvent/src/main/java/com/ApiEvent/config/MailSender.java
+++ ApiEvent/src/main/java/com/ApiEvent/config/MailSender.java
@@ -7,7 +7,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 public class MailSender {
-	@Bean
+	
 	public JavaMailSender getJavaMailSender() {
 	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 	    mailSender.setHost("smtp.gmail.com");
