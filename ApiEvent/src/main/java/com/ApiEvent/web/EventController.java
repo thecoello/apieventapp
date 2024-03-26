53463ac6d84a781af6ffc8bd4bc33480dae56139
thecoello <thecoello@gmail.com>
2024-03-26 19:52:34 +0100

    Permiso de cots con front y back

--- ApiEvent/src/main/java/com/ApiEvent/web/EventController.java
+++ ApiEvent/src/main/java/com/ApiEvent/web/EventController.java
@@ -1,6 +1,5 @@
 package com.ApiEvent.web;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -9,28 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ApiEvent.domain.Event;
-import com.ApiEvent.domain.Zone;
 import com.ApiEvent.service.EventService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
 
-import aj.org.objectweb.asm.Type;
-
+@CrossOrigin(origins ="http://localhost:4200") 
 @RestController
 public class EventController {
 
